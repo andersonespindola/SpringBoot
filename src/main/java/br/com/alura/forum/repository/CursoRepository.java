2dcab5cf1c8cf7ce9415464bac341e0936431a79
Anderson Espindola <anderson.espindola@senior.com.br>
2020-02-28 08:52:21 -0300

    funcionando todos os metodos, post, put e delete

--- src/main/java/br/com/alura/forum/repository/CursoRepository.java
+++ src/main/java/br/com/alura/forum/repository/CursoRepository.java
@@ -1,9 +1,11 @@
 package br.com.alura.forum.repository;
 
-import br.com.alura.forum.modelo.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CursoRepository extends JpaRepository<Curso, Long> {
-    Curso findByNome(String nome);
-}
+import br.com.alura.forum.modelo.Curso;
 
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+	Curso findByNome(String nome);
+
+}
